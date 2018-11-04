b79ecacca2aa260fe4d29c0c2af2db654762c9d9
Aliaksei Kuchyn <forrana@gmail.com>
2018-11-04 22:21:49 +0200

    Remove unused client files

--- client/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import  { shallow } from "enzyme";
-import * as React from 'react';
-import App from './App';
-
-it('renders without crashing', () => {
-  const component = shallow(<App />);
-  expect(component).toMatchSnapshot();
-});
--- client/src/setupTests.ts
+++
@@ -1,4 +0,0 @@
-import { configure } from 'enzyme';
-// @ts-ignore
-import * as EnzymeAdapter from 'enzyme-adapter-react-16';
-configure({ adapter: new EnzymeAdapter() });
