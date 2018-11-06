f36a8031de3578bbbb8a7259d9403ce31a45b01f
Aliaksei Kuchyn <forrana@gmail.com>
2018-11-06 22:19:42 +0200

    Add attribute, skill models, add TimeStampsOwnerModel mixin to make code more DRY

--- server/app/models.py
+++ server/app/models.py
@@ -5,9 +5,9 @@ from flask_user import UserMixin, UserManager
 db = MongoEngine(app)
 
 def fill_timestamps(self):
-    if not self.creation_date:
-        self.creation_date = datetime.now()
-    self.modified_date = datetime.now()
+    if not self.created_at:
+        self.created_at = datetime.now()
+    self.updated_at = datetime.now()
     return self
 
 class User(db.Document, UserMixin):
@@ -18,29 +18,48 @@ class User(db.Document, UserMixin):
     password = db.StringField()
     email = db.StringField()
     password = db.StringField()
-    creation_date = db.DateTimeField()
-    modified_date = db.DateTimeField()
+    created_at = db.DateTimeField()
+    updated_at = db.DateTimeField()
     roles = db.ListField(db.StringField(), default=[])
 
     def save(self, *args, **kwargs):
-        self = fill_timestamps(self)
+        fill_timestamps(self)
         return super(User, self).save(*args, **kwargs)
 
 user_manager = UserManager(app, db, User)
 
-class Expense(db.Document):
-    amount = db.StringField()
-    date = db.DateTimeField()
-    time = db.StringField()
-    name = db.StringField()
-    price = db.FloatField()
-    place = db.StringField()
-    tags = db.ListField(db.StringField(), default=[])
-    creation_date = db.DateTimeField()
-    modified_date = db.DateTimeField()
+
+class TimeStampsOwnerModel(db.Document):
+    meta = {
+        'abstract': True
+    }
+
+    updated_at = db.DateTimeField(default=datetime.now)
+    created_at = db.DateTimeField(default=datetime.now)
     owner = db.ReferenceField(User)
 
     def save(self, *args, **kwargs):
-        self = fill_timestamps(self)
+        fill_timestamps(self)
         self.owner = current_user.id
-        return super(Expense, self).save(*args, **kwargs)
+        return super(TimeStampsOwnerModel, self).save(*args, **kwargs)
+
+class Attribute(TimeStampsOwnerModel):
+    name = db.StringField()
+
+class Skill(TimeStampsOwnerModel):
+    name = db.StringField()
+    description = db.StringField()
+    target_day = db.FloatField(default=0)
+    target_week = db.FloatField(default=0)
+    target_month = db.FloatField(default=0)
+    target_year = db.FloatField(default=0)
+    attribute = db.ReferenceField(Attribute)
+
+class Expense(TimeStampsOwnerModel):
+    amount = db.StringField()
+    date = db.DateTimeField()
+    time = db.StringField()
+    place = db.StringField()
+    note = db.StringField()
+    tags = db.ListField(db.StringField(), default=[])
+    skill = db.ReferenceField(Skill)
