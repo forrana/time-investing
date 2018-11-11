04b1fe17d927b2aadecd6030441f0231e75debfc
Aliaksei Kuchyn <forrana@gmail.com>
2018-11-11 23:51:35 +0200

    Progress bar and start button logic

--- server/app/templates/js/expanses-list.js
+++ server/app/templates/js/expanses-list.js
@@ -12,3 +12,24 @@ setInterval(() => {
   else location.reload();
   timeLeftElement.innerHTML = minutesLeftInTheCurrentDay
 }, 1000*60);
+
+
+const onActivityStart = () => {
+  let timerProgress = 0;
+  const timeProgressBar = document.querySelector("#time-progressbar");
+  const amountOfTime = document.querySelector("#amount-of-time").value * 60;
+  const timeProgressBarText = document.querySelector("#time-progressbar-text");
+  const percentsOfTimePerSecond = 100/(+amountOfTime);
+
+  timeProgressBarText.innerHTML = `${timerProgress}/${amountOfTime} seconds`;
+
+  const timeProgresseInterval = setInterval(() => {
+    timerProgress++;
+    if(timerProgress >= amountOfTime) {
+      clearInterval(timeProgresseInterval)
+    }
+    timeProgressBar.setAttribute("aria-valuenow", timerProgress*percentsOfTimePerSecond);
+    timeProgressBar.style.width = `${timerProgress*percentsOfTimePerSecond}%`;
+    timeProgressBarText.innerHTML = `${timerProgress}/${amountOfTime} seconds`;
+  }, 1000)
+}
